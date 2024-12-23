c3266552215bf9893d721ebcceb415c770d9c603
Jerome Duncan <hello@itsjero.me>
2024-12-23 12:29:00 +0000

    fix: missed minus

--- src/main.rs
+++ src/main.rs
@@ -198,6 +198,7 @@ impl Lexer {
                 Some('.') => Some(Token::new(TokenType::Period, String::from("."), Some(Value::Null))),
                 Some(',') => Some(Token::new(TokenType::Comma, String::from(","), Some(Value::Null))),
                 Some('+') => Some(Token::new(TokenType::Plus, String::from("+"), Some(Value::Null))),
+                Some('-') => Some(Token::new(TokenType::Minus, String::from("-"), Some(Value::Null))),
                 None => Some(Token::new(TokenType::Eof, String::from(""), Some(Value::Null))),
                 _ => None,
             };
