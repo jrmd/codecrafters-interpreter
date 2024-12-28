dd4e5d0445503663b7e18cd9e5a63de038dca94e
Jerome Duncan <hello@itsjero.me>
2024-12-28 23:01:56 +0000

    fuck off

--- src/main.rs
+++ src/main.rs
@@ -808,7 +808,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                 parser.dump();
             } else {
                 let res = res.err().unwrap();
-                println!("{res}\n");
+                println!("\n{res}\n");
                 std::process::exit(65);
             }
         }
